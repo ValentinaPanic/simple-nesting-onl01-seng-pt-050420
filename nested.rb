5bf127d904893ef821711ce2d6331ca502eaa8a2
Valentina Panic <valentina.panic87@gmail.com>
2020-05-15 23:58:36 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -68,6 +68,7 @@ def adding_matz
 
 	programmer_hash =
  		{
+			::yukihiro_matsumoto => {}
         :grace_hopper => {
           :known_for => "COBOL",
           :languages => ["COBOL", "FORTRAN"]
@@ -82,7 +83,7 @@ def adding_matz
         }
      }
 
-programmer_hash[:yukihiro_matsumoto => :known_for => "Ruby", :languages => ["LISP", "C"]]
+programmer_hash
 end
 
 def changing_alan
